6f90472d32f43179ba3823926dfe32f68b9d6b0d
hc073q <hc073q@bdcoes-mbp-6.gateway.pace.com>
2014-12-24 16:07:43 -0800

    give default fellowship link

--- public/app/main/MainCtrl.js
+++ public/app/main/MainCtrl.js
@@ -1,13 +1,14 @@
 //This code stores all records into mvMainCtrl controller
-angular.module('app').controller('MainCtrl', function ($scope,IdentitySvc) {
-	$scope.popover = {
-		"title": "Title",
-		"content": "Hello Popover<br />This is a multiline message!"
-	};
-
-
-
+angular.module('app').controller('MainCtrl', function ($scope,IdentitySvc,$http,$location) {
 	console.log("IdentitySvc.isAuthenticated()");
 	console.log(IdentitySvc.currentUser);
 	$scope.IdentitySvc = IdentitySvc;
+
+	$scope.logout = function(){
+		console.log("logout");
+		$http.post("/logout").success(function(){
+			$location.path("/");
+			IdentitySvc.currentUser = null;
+		});
+	};
 });
