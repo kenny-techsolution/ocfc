c80db7f4dbde10bf1d48f66394bac2622e3e259c
Kenny Chung <yoyocicada@gmail.com>
2014-12-25 17:06:53 -0800

    update code

---
+++ public/app/fellowship/FellowshipMembersCtrl.js
@@ -0,0 +1,6 @@
+angular.module('app').controller('FellowshipMembersCtrl', function ($http, $scope,
+                                                                   IdentitySvc, FellowshipSvc,FellowshipUserSvc, $routeParams,
+                                                                   mySocket, $timeout, GoogleMapGeocoderSvc) {
+
+	
+});
