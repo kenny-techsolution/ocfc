20005341560e428ee39f6c51a9f78f472c674393
hc073q <hc073q@bdcoes-mbp-6.gateway.pace.com>
2014-12-29 21:34:35 -0800

    fix code

---
+++ public/app/fellowship/ApproveFellowshipCtrl.js
@@ -0,0 +1,30 @@
+angular.module('app').controller('FellowshipCtrl', function ($http, $scope,
+                                                             IdentitySvc, FellowshipSvc,FellowshipDataSvc,FellowshipUserSvc, $routeParams,
+                                                             mySocket, $timeout) {
+	$scope.fellowshipName;
+	$scope.churchName;
+	$scope.street="";
+	$scope.city="";
+	$scope.state="";
+	$scope.zipcode="";
+	$scope.country="";
+	$scope.phone;
+	$scope.aboutFellowship;
+
+	//Populate all fellowships to approve
+	$scope.fellowships = FellowshipSvc.query(function () {
+			console.log('chk $scope.fellowships');
+			console.log($scope.fellowships);
+		}
+	);
+	$scope.approveFellowship=function(fellowship){
+		console.log('front-end approveFellowship has been called');
+		console.log('chk fellowship');
+		console.log(fellowship);
+		//call updateFellowshipById
+		fellowship.approved=true;
+		FellowshipSvc.update({id:fellowship._id},fellowship);
+
+	};
+
+});
