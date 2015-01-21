25e15e5457dac08d67037af185680a992f9c006f
Kenny Chung <yoyocicada@gmail.com>
2015-01-21 15:43:41 -0800

    update announcement widget func

--- public/app/common/ocfc-announcement.js
+++ public/app/common/ocfc-announcement.js
@@ -11,6 +11,15 @@ angular.module('app').directive('ocfcAnnouncement', function (IdentitySvc, Comme
 		templateUrl: '/partials/common/ocfc-announcement',
 		controller: function ($scope) {
 			console.log('ocfcAnnouncement has been called');
+			$scope.IdentitySvc = IdentitySvc;
+			$scope.showEdit = false;
+			$scope.newAnnouncePostContent = $scope.post.announcement[0].content;
+
+
+			$scope.hideEditPost = function () {
+				console.log('hideEditPost function called');
+				$scope.showEdit = false;
+			};
 
 			//edit post derived from dropdown
 			$scope.editPost = function () {
@@ -44,7 +53,30 @@ angular.module('app').directive('ocfcAnnouncement', function (IdentitySvc, Comme
 				});
 
 			};
+			$scope.updateEditedPost = function () {
+				//console.log('chk $scope.post obj');
+				//console.log($scope.post);
+				console.log('updateEditedPost function called');
+				$scope.post.announcement[0].content = $scope.newAnnouncePostContent;
 
+				//cannot update post other than your own
+				if ($scope.post.postBy._id === IdentitySvc.currentUser._id) {
+					console.log('if condition is met, this post is made by current user');
+
+					var updatePost = angular.copy($scope.post);
+					//do not allow update on images
+					delete updatePost.imageIds;
+					updatePost.postType = "announcement";
+
+					//update post obj on the server side
+					PostSvc.update({id: updatePost._id}, updatePost, function () {
+						console.log('front-end PostSvc.update has completed');
+					});
+					$scope.showEdit = false;
+				} else {
+					alert('Sorry, you do no have rights to update post other than your own');
+				}
+			};
 
 		}
 	};
